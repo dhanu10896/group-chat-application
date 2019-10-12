c3284d2801a246f3e0847cec233032061fbea930
Dhananjay Nagargoje <dhananjay.nagargoje@sap.com>
2019-10-13 02:42:59 +0530

    group chat application

---
+++ src/main/java/com/fullstackwebapp/chatapp/SpringBootGroupChatApplication.java
@@ -0,0 +1,17 @@
+package com.fullstackwebapp.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootGroupChatApplication {
+
+    public static void main(String[] args) {
+        /// Register Servlet.
+
+        SpringApplication.run(SpringBootGroupChatApplication.class, args);
+
+    }
+
+}
